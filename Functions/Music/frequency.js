067cfcddabce3d4e8ff09805daaebf5e6ee74c18
Gary Yamashita <garyjyam@gmail.com>
2026-09-02 17:52:44 -0700

    findFrequency helper finished

--- Functions/Music/frequency.js
+++ Functions/Music/frequency.js
@@ -1,4 +1,12 @@
-const notesSharps = [
+// fn = fo * (a) ** n
+// frequency of note = refValue * (2 ** (1 /12)) ** n
+// we need to solve for n (find number of semitones from reference note)
+// first we need to determine if we are using sharps or flats array
+// next we need to determine directional flow
+// if the note is above A and octave is 4 we go forward.
+// we then iterate until we get to the note, incrementing a variable to use for n, raising or lowering the octave at C, depending on our direction
+// once we have n we plug it in and return the answer
+const sharpsArray = [
   "A",
   "A#",
   "B",
@@ -12,7 +20,7 @@ const notesSharps = [
   "G",
   "G#",
 ];
-const notesFlats = [
+const flatsArray = [
   "A",
   "Bb",
   "B",
@@ -26,67 +34,56 @@ const notesFlats = [
   "G",
   "Ab",
 ];
-
-// fn = fo * (a) ** n
-// frequency of note = refValue * (2 ** (1 /12)) ** n
-// we need to solve for n (find number of semitones from reference note)
-// first we need to determine if we are using sharps or flats array
-// next we need to determine if the note is above or below ref value. (If pitch and octave equals A4 we return the ref value)
-// we then iterate until we get to the note, incrementing a variable to use for n, raising or lowering the octave at C, depending on our direction
-// once we have n we plug it in and return the answer
-
-export function findNoteFrequency(pitch, octave, refValue) {
-  let useFlats = false;
-  let isAbove = false;
-  let currentOctave = 4;
-
-  if (
-    pitch === "Ab" ||
-    pitch === "Bb" ||
-    pitch === "Db" ||
-    pitch === "Eb" ||
-    pitch === "Gb"
-  ) {
-    useFlats = true;
-  }
-
-  if (pitch === "A#" || pitch === "Bb" || pitch === "B") {
-    isAbove = true;
-  }
-
-  if (useFlats) {
-    // forward rotation
-    if (octave >= 5 || (octave === 4 && isAbove)) {
-      for (let i = 0; i < notesFlats.length; i++) {
-        if (note === "C") {
-          currentOctave++;
-        }
-      }
-    } else {
-      // reverse rotation
-      for (let i = notesFlats.length - 1; i >= 0; i--) {
-        if (note === "C") {
-          currentOctave--;
-        }
-      }
+// helper
+function findNumOfSemitones(pitch, octave, arr, direction) {
+  let cIndex = 0;
+  let cOctave = 4;
+  let n = 0;
+  let max = 200;
+  let len = arr.length;
+  // make sure we get forward or reverse!
+  if (direction !== "forward" && direction !== "reverse") return;
+  if (direction === "forward") {
+    while ((arr[cIndex] !== pitch || cOctave !== octave) && n < max) {
+      if (arr[cIndex] === "C") cOctave++;
+      cIndex = (cIndex + 1) % len;
+      n++;
     }
   } else {
-    // forward rotation
-    if (octave >= 5 || (octave === 4 && isAbove)) {
-      for (let i = 0; i < notesFlats.length; i++) {
-        if (note === "C") {
-          currentOctave++;
-        }
-      }
-      if (octave >= 5 || (octave === 4 && isAbove)) {
-      } else {
-        // reverse rotation
-        for (let i = notesFlats.length - 1; i >= 0; i--) {
-          if (note === "C") {
-            currentOctave--;
-          }
-        }
-      }
+    while ((arr[cIndex] !== pitch || cOctave !== octave) && n > -max) {
+      if (arr[cIndex] === "C") cOctave--;
+      cIndex = (cIndex - 1 + len) % len;
+      n--;
     }
   }
+  return n;
 }
+// main function
+export function findFrequency(pitch, octave, refValue = 440) {
+  // return the ref value if note is A4
+  if (pitch === "A" && octave === 4) return refValue;
+  // convert string version to symbols
+  let note = pitch[0].toUpperCase();
+  let extension = pitch.slice(1).toLowerCase();
+  if (extension === "flat") extension = "b";
+  if (extension === "sharp") extension = "#";
+  if (extension && extension !== "b" && extension !== "#") {
+    console.log(`Invalid note extension`);
+    return;
+  }
+  let cleanedNote = note + extension;
+  // set flags and determine logical variables
+  let useFlats = cleanedNote[1] === "b";
+  let isAboveA =
+    cleanedNote === "A#" || cleanedNote === "Bb" || cleanedNote === "B";
+  let isForward = octave > 4 || (octave === 4 && isAboveA);
+  let direction = isForward ? "forward" : "reverse";
+  let arr = useFlats ? flatsArray : sharpsArray;
+  let n = findNumOfSemitones(cleanedNote, octave, arr, direction);
+  // solve for freqency (frequency = refValue * a ** n)
+  let a = 2 ** (1 / 12); //12th root of 2
+  let calculatedFrequency = refValue * a ** n;
+  return calculatedFrequency;
+}
+
+console.log(findFrequency("dsharp", 5));
