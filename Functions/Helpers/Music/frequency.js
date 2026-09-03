9588d80b8cb9e81d33c5b0e6d6e9ecbb1ba77848
Gary Yamashita <garyjyam@gmail.com>
2026-09-03 09:53:20 -0700

    findMidiValue created

--- Functions/Helpers/Music/frequency.js
+++ Functions/Helpers/Music/frequency.js
@@ -26,15 +26,15 @@ const flatsArray = [
   "G",
   "Ab",
 ];
-
 // fn = fo * (a) ** n
 // frequency of note = refValue * (2 ** (1 /12)) ** n
 // we need to solve for n (find number of semitones from reference note)
 // first we need to determine if we are using sharps or flats array
 // next we need to determine directional flow
-// if the note is above A and octave is 4 we go forward.
+// octaves above 4 go forward, below 4 and we go reverse
+// if the octave is 4 and the note is above A, we go forward.
 // we then iterate until we get to the note, incrementing a variable to use for n, raising or lowering the octave at C, depending on our direction
-// once we have n we plug it in and return the answer
+// once we have n, we plug it in and return the answer
 // helper
 function findNumOfSemitones(pitch, octave, arr, direction) {
   let cIndex = 0;
@@ -59,11 +59,8 @@ function findNumOfSemitones(pitch, octave, arr, direction) {
   }
   return n;
 }
-// main function
-export function findFrequency(pitch, octave, refValue = 440) {
-  // return the ref value if note is A4
-  if (pitch === "A" && octave === 4) return refValue;
-  // convert string version to symbols
+// convert string version to symbols
+function convertToMusicalSymbols(pitch) {
   let note = pitch[0].toUpperCase();
   let extension = pitch.slice(1).toLowerCase();
   if (extension === "flat") extension = "b";
@@ -73,45 +70,45 @@ export function findFrequency(pitch, octave, refValue = 440) {
     return;
   }
   let cleanedNote = note + extension;
-  // set flags and determine logical variables
-  let useFlats = cleanedNote[1] === "b";
+  return cleanedNote;
+}
+// determine direction from A4
+function determineDirection(pitch, octave) {
+  let cleanedNote = convertToMusicalSymbols(pitch);
   let isAboveA =
     cleanedNote === "A#" || cleanedNote === "Bb" || cleanedNote === "B";
   let isForward = octave > 4 || (octave === 4 && isAboveA);
   let direction = isForward ? "forward" : "reverse";
+  return direction;
+}
+function determineArray(pitch) {
+  let cleanedNote = convertToMusicalSymbols(pitch);
+  let useFlats = cleanedNote[1] === "b";
   let arr = useFlats ? flatsArray : sharpsArray;
+  return arr;
+}
+// main function
+export function findFrequency(pitch, octave, refValue = 440) {
+  // return the ref value if note is A4
+  if (pitch === "A" && octave === 4) return refValue;
+  let cleanedNote = convertToMusicalSymbols(pitch);
+  // set flags and determine logical variables
+  let direction = determineDirection(cleanedNote, octave);
+  let arr = determineArray(cleanedNote);
   let n = findNumOfSemitones(cleanedNote, octave, arr, direction);
   // solve for freqency (frequency = refValue * a ** n)
-  let a = 2 ** (1 / 12); //12th root of 2
+  let a = 2 ** (1 / 12); // 12th root of 2
   let calculatedFrequency = refValue * a ** n;
   return calculatedFrequency;
 }
-
 console.log(findFrequency("dsharp", 5));
-
-/*Middle C (C4) is universally standardized as Note 60.
-Lower notes drop down in semitones (e.g., B3 is 59, A3 is 57).
-Higher notes step up in semitones (e.g., C#4 is 61, D4 is 62).
-0 is the bottom and it corresponds to C-2 in most DAWs and hardware setups.
-The absolute top is 127 which corresponds to G8.*/
-
-// we need to iterate from C
-export function findMidiValue(pitch, octave) {
-  // change words to symbols and guard
-  let note = pitch[0].toUpperCase();
-  let extension = pitch.slice(1).toLowerCase();
-  if (extension === "flat") extension = "b";
-  if (extension === "sharp") extension = "#";
-  if (extension && extension !== "b" && extension !== "#") {
-    console.log(`Invalid note extension`);
-    return;
-  }
-  let cleanedNote = note + extension;
-  // set flags and determine logical variables
-  let useFlats = cleanedNote[1] === "b";
-  let arrToUse = useFlats ? flatsArray : sharpsArray;
-  let isReverse = octave < 4;
-  let cIndex = 3; // C is index 3
-  let cOctave = 4;
-  let midiValue = 60; // midi value of C4
+// A4 is Note 69
+// I'm using my helpers for this as well since it's all used together in my factory functions
+function findMidiValue(pitch, octave) {
+  let cleanedNote = convertToMusicalSymbols(pitch);
+  let direction = determineDirection(cleanedNote, octave);
+  let arr = determineArray(cleanedNote);
+  let n = findNumOfSemitones(cleanedNote, octave, arr, direction);
+  return 69 + n;
 }
+console.log(findMidiValue("E", 3));
