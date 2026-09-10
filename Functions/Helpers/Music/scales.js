b8c958d82948f40ff88f7ea5d1d5ac5429487a14
Gary Yamashita <garyjyam@gmail.com>
2026-09-10 04:09:24 -0700

    scales added

--- Functions/Helpers/Music/scales.js
+++ Functions/Helpers/Music/scales.js
@@ -1,11 +1,15 @@
 export const scaleMap = {
   // ionian = major
-  ionian: [0, 2, 4, 5, 7, 9, 11], // TTSTTTS
-  dorian: [0, 2, 3, 5, 7, 9, 10], // TSTTTST
-  phrygian: [0, 1, 3, 5, 7, 8, 10], // TSTTTST
-  lydian: [0, 2, 4, 6, 7, 9, 11], // TTTSTTS
-  mixolydian: [0, 2, 4, 5, 7, 9, 10], // TTSTTST
+  ionian: [0, 2, 4, 5, 7, 9, 11], // T,T,S,T,T,T,S
+  dorian: [0, 2, 3, 5, 7, 9, 10], // T,S,T,T,T,S,T
+  phrygian: [0, 1, 3, 5, 7, 8, 10], // T,S,T,T,T,S,T
+  lydian: [0, 2, 4, 6, 7, 9, 11], // T,T,T,S,T,T,S
+  mixolydian: [0, 2, 4, 5, 7, 9, 10], // T,T,S,T,T,S,T
   // aeolian = minor pure
-  aeolian: [0, 2, 3, 5, 7, 8, 10], // TSTTSTT
-  locrian: [0, 1, 3, 5, 6, 8, 10], // STTSTTT
+  aeolian: [0, 2, 3, 5, 7, 8, 10], // T,S,T,T,S,T,T
+  locrian: [0, 1, 3, 5, 6, 8, 10], // S,T,T,S,T,T.T
+  minorPentatonic: [0, 3, 5, 7, 10], // TS,T,T,TS,T
+  majorPentatonic: [0, 2, 4, 7, 9], // T,T,TS,T,TS
+  harmonicMinor: [0, 2, 3, 5, 7, 8, 11], // T,S,T,T,S,TS,S
+  melodicMinor: [0, 2, 3, 5, 7, 9, 11], // T,S,T,T,T,T,S
 };
